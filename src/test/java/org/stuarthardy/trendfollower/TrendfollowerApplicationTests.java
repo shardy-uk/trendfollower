ef1b2a0b21f0e6f6c7a1f4bb42915b816d839f15
shardy <stuart@stuarthardy.org>
2022-05-04 16:57:38 +0100

    Initial Commmit

---
+++ src/test/java/org/stuarthardy/trendfollower/TrendfollowerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.stuarthardy.trendfollower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrendfollowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
